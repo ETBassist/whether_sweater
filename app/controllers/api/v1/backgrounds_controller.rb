ac96af592ec3951b3d66598b2331a22bb15f58df
Eugene Theriault <hybridbassist@gmail.com>
2021-01-16 21:47:48 -0800

    Create image endpoint

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class BackgroundsController < ApplicationController
       def search
-
+        image = ImageFacade.image_search(params[:location])
+        render json: ImageSerializer.new(image)
       end
     end
   end
