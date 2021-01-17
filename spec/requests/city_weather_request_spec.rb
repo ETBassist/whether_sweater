f302a80c5eaf4b892ac6905c1835ba7a66ca3ef2
Eugene Theriault <hybridbassist@gmail.com>
2021-01-16 18:00:38 -0800

    Add tests for things a response shouldn't have

--- spec/requests/city_weather_request_spec.rb
+++ spec/requests/city_weather_request_spec.rb
@@ -7,7 +7,6 @@ describe '/api/v1/forecast?location=denver,co', :vcr do
         get '/api/v1/forecast?location=washington,dc'
 
         result = JSON.parse(response.body, symbolize_names: true)
-        require "pry";binding.pry 
 
         expect(result).to have_key(:data)
         expect(result[:data]).to have_key(:id)
---
+++ spec/serializers/forecast_serializer_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'ForecastSerializer' do
+  it 'returns a serialized Forecast without unwanted attributes' do
+    VCR.use_cassette('forecast_serializer_spec') do
+      forecast = ForecastFacade.create_forecast_for_coordinates(38.8921, -77.0199)
+      formatted_forecast = ForecastSerializer.serialize_forecast(forecast)
+
+      expect(formatted_forecast).to have_key(:data)
+      expect(formatted_forecast[:data]).to have_key(:id)
+      expect(formatted_forecast[:data][:id]).to eq(nil)
+      expect(formatted_forecast[:data]).to have_key(:type)
+      expect(formatted_forecast[:data][:type]).to eq('forecast')
+
+      expect(formatted_forecast[:data][:attributes]).to have_key(:current_weather)
+      expect(formatted_forecast[:data][:attributes]).to have_key(:daily_weather)
+      expect(formatted_forecast[:data][:attributes][:daily_weather]).to be_an(Array)
+      expect(formatted_forecast[:data][:attributes]).to have_key(:hourly_weather)
+      expect(formatted_forecast[:data][:attributes][:hourly_weather]).to be_an(Array)
+
+      formatted_forecast[:data][:attributes][:daily_weather].each do |day|
+        expect(day).to_not have_key(:feels_like)
+        expect(day).to_not have_key(:pressure)
+        expect(day).to_not have_key(:dew_point)
+        expect(day).to_not have_key(:clouds)
+      end
+
+      formatted_forecast[:data][:attributes][:hourly_weather].each do |hour|
+        expect(hour).to_not have_key(:feels_like)
+        expect(hour).to_not have_key(:pressure)
+        expect(hour).to_not have_key(:dew_point)
+        expect(hour).to_not have_key(:clouds)
+      end
+    end
+  end
+end
