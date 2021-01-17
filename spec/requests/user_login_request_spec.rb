c184ffd34c2ea8639dfb1a47c483bc0dac4bc9ec
Eugene Theriault <hybridbassist@gmail.com>
2021-01-17 14:59:43 -0800

    Create test for user login endpoint

---
+++ spec/requests/user_login_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'User Log In request' do
+  it 'can log a user in with valid credentials' do
+    user = create(:user)
+
+    header_data = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    body_data = {
+      email: user.email,
+      password: user.password
+    }
+
+    post '/api/v1/session', params: { headers: header_data, body: body_data }
+
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(result).to be_successful
+    expect(result).to have_key(:data)
+    expect(result[:data]).to have_key(:type)
+    expect(result[:data][:type]).to eq('users')
+    expect(result[:data]).to have_key(:id)
+    expect(result[:data][:id]).to eq(users.id.to_s)
+    expect(result[:data]).to have_key(:attributes)
+    expect(result[:data][:attributes]).to have_key(:email)
+    expect(result[:data][:attributes][:email]).to eq(user.email)
+    expect(result[:data][:attributes]).to have_key(:api_key)
+    expect(result[:data][:attributes][:api_key]).to eq(user.api_key)
+  end
+end
