8a172a05d2d5cd8514b1713cb87c4237ce1c6f09
Eugene Theriault <hybridbassist@gmail.com>
2021-01-18 14:57:38 -0800

    Add logic for impossible RoadTrip weather_at_eta
    Sets weather_at_eta to empty has in event that a trip cannot be made

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -4,12 +4,8 @@ class RoadTripFacade
       coords = CoordinatesFacade.create_coordinates(params_body[:destination])
       time = TimeFacade.query_direction_time_data(params_body[:origin], params_body[:destination])
       weather = OpenWeatherService.get_forecast(coords[:lat], coords[:lng])
-      arrival_hour = (Time.at(weather[:current][:dt]) + time.time_in_seconds.seconds).strftime("%H")
 
-      forecast = weather[:hourly].find do |hour|
-        Time.at(hour[:dt]).strftime("%H") == arrival_hour
-      end
-      RoadTrip.new(params_body, time.travel_time, forecast)
+      RoadTrip.new(params_body, time, weather)
     end
   end
 end
