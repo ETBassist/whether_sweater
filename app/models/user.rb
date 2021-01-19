976695ed9a2f8a003ffbe55ab1081d56f3d874b8
Eugene Theriault <hybridbassist@gmail.com>
2021-01-18 17:50:38 -0800

    Switch to using has_secure_token in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_secure_token :api_key
 
-  validates :email, :api_key, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :api_key, uniqueness: true
 end
