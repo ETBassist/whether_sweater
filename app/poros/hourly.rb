d48ca4f71cb6c40ae0b40740d77b3b5c966f2f1f
Eugene Theriault <hybridbassist@gmail.com>
2021-01-19 12:30:28 -0800

    Create Hourly weather class and spec

---
+++ app/poros/hourly.rb
@@ -0,0 +1,25 @@
+class Hourly
+  attr_reader :time,
+              :temperature,
+              :wind_speed,
+              :wind_direction,
+              :conditions,
+              :icon
+
+  def initialize(hour)
+    @time = Time.at(hour[:dt]).to_s
+    @temperature = hour[:temp]
+    @wind_speed = "#{ hour[:wind_speed] } mph"
+    @wind_direction = calculate_direction(hour[:wind_deg])
+    @conditions = hour[:weather].first[:description]
+    @icon = hour[:weather].first[:icon]
+  end
+  
+  private
+
+  def calculate_direction(wind_deg)
+    compass = %w(N NNE NE ENE E ESE SE SSE S SSW SW WSW W WNW NW NNW)
+    compass_sector = (wind_deg / 22.5) + 0.5
+    compass[(compass_sector % 16).round]
+  end
+end
